c000af4cbab1c24756029e91b5d7672a4219930e
Bill_Chow <735051883@qq.com>
2020-07-12 15:11:10 +0800

    Site updated: 2020-07-12 15:11:10

--- js/script.js
+++ js/script.js
@@ -82,7 +82,15 @@ document.ready(
         };
         _Blog.toggleTheme();
 
-        // ready function.
+
+        /* Apply fancybox to multiple items */
+        $("a.group").fancybox({
+            'transitionIn'	:	'elastic',
+            'transitionOut'	:	'elastic',
+            'speedIn'		:	600, 
+            'speedOut'		:	200, 
+            'overlayShow'	:	false
+        });
 
     }
 );
